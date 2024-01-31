aa220b765ff05c8567c626ff56569a36a852de48
lfouuu <boukharifourat61@gmail.com>
2024-01-31 16:06:22 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import AddTask from './Components/AddTask';
+import ListTasks from './Components/ListTasks';
+
+function App() {
+  
+  return (
+    <div>
+      <ListTasks/>
+      <br/>
+      <AddTask/>
+    </div>
+  );
+}
+
+export default App;
